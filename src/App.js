a670499a9983a7e17d69f3b54ad9f696f7328a07
Ryan Morton <morton.ryan92@gmail.com>
2019-02-04 16:45:17 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      points: [
+        { name: "Eric", points: 27 },
+        { name: "Jordan", points: 26 },
+        { name: "Darien", points: 20 },
+        { name: "Ryan", points: 16 },
+        { name: "Mariam", points: 8 },
+        { name: "Courtney", points: 4 },
+        { name: "David", points: 2 },
+        { name: "Madeline", points: 1 },
+        { name: "Meg", points: 1 },
+        { name: "Nick", points: 1 },
+        { name: "Eric W.", points: 1 }
+      ]
+    };
+  }
+
   render() {
+    const people = this.state.points
+      .sort((a, b) => {
+        return a.points < b.points;
+      })
+      .map(p => {
+        return <li>{`${p.name}: ${p.points}`}</li>;
+      });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Point Leaderboard</h1>
+        <ul>{people}</ul>
       </div>
     );
   }
