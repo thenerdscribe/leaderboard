00ce3bcdb68a2d06b2ded53909017263970ccc01
Ryan Morton <ryanmorton@Kevin-iMac.local>
2019-04-09 17:28:05 -0700

    Updates

---
+++ precache-manifest.a23c384a0e76405d252535aa91d6f579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2efabc4d57a488c6b4b",
+    "url": "/leaderboard/static/css/main.7654f5f9.chunk.css"
+  },
+  {
+    "revision": "b2efabc4d57a488c6b4b",
+    "url": "/leaderboard/static/js/main.b17cc06a.chunk.js"
+  },
+  {
+    "revision": "25a1a5afe6c1454de7ae",
+    "url": "/leaderboard/static/js/runtime~main.e3d67ba9.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/leaderboard/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5d8b85c726b97c2ee0857430e5aaa11b",
+    "url": "/leaderboard/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/leaderboard/precache-manifest.9fec44fde1146c556088cea5bd89e9c8.js"
+  "/leaderboard/precache-manifest.a23c384a0e76405d252535aa91d6f579.js"
 );
 
 workbox.clientsClaim();
